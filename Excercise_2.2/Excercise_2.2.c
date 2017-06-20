247a031dff5ef58a6dc973242572a1e3df3e9e8e
sunrise <tim_melnikov@mail.ru>
2017-06-20 13:57:46 +0300

    Excercise 2.2 completed.

---
+++ Excercise_2.2/Excercise_2.2.c
@@ -0,0 +1,67 @@
+/*Excercise 2.2 from K&R*/
+/* Write "for" construction equal to
+ * "for (i = 0; i < lim-1 && (с = getchar()) != EOF && с != '\n'; ++i)
+ * s[i] = c;
+ * without using "&&" and "||" */
+
+/* I'll copy the full code, incuding "for", and change this part
+ * this is original code*/
+
+#include <stdio.h>
+#define MAXLINE 1000 /* maximum size of line */
+int getline(char linef[], int );
+void copy(char to[], char fromf[]);
+
+/* printing the longest line */
+main()
+{
+    int len; /* current line's length */
+    int max; /* max length */
+    char line[MAXLINE]; /* current line */
+    char longest[MAXLINE]; /* the longest line */
+    max = 0;
+
+    while ((len = getline(line, MAXLINE)) > 0)
+        if (len > max) {
+            max = len;
+            copy(longest, line);
+        }
+
+    if (max > 0) /* was there any strings? */
+        printf("%s", longest);
+    return 0;
+}
+
+
+/* getline: reads a line of s, returns length */
+int getline(char s[], int lim)
+{
+    int c, i;
+
+    /*this is a part we need to change */
+    for (i = 0; i < lim-1; ++i)
+        if((c = getchar()) != EOF) /*we can check "for" conditions using "if-then-else"*/
+            if(с != '\n')/*If condition is false, we use "break"*/
+                s[i] = c;
+            else break;
+        else break;
+
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+
+    s[i] = '\0';
+    return i;
+}
+
+
+/* copy: copy from 'from' to 'to'; to is big enough */
+void copy(char to[], char from[])
+{
+    int i;i = 0;
+
+    while ((to[i] = from[i]) != '\0')
+        ++i;
+}
+
