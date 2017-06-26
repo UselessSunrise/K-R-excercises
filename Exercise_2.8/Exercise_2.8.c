44e388a917a7bc9ac74e6e21dd024cdf8e806bb2
sunrise <tim_melnikov@mail.ru>
2017-06-26 14:23:54 +0300

    Exercise 2.8 .

---
+++ Exercise_2.8/Exercise_2.8.c
@@ -0,0 +1,12 @@
+/*Exercise 2.7 from K&R*/
+/* Write function rightrot(х, n) which cyclically moves "x" to the right by the "n" digits */
+
+unsigned rightrot(unsigned x, int n);
+
+unsigned rightrot(unsigned x, int n){
+    for(int i = 1; i <= n; i++){
+        (x << 1);
+    }
+
+    return x; /* This is how I understand the task, maybe it's not the correct idea */
+}
